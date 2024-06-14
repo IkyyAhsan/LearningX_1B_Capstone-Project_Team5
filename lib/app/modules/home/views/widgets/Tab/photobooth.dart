23b3e779b540bd50e22939c8359cbed765c970c6
Marlia Putri <marliaputri@MacBook-Air-Marlia.local>
2024-06-14 20:57:13 +0700

    update photoboth

--- lib/app/modules/home/views/widgets/Tab/photobooth.dart
+++ lib/app/modules/home/views/widgets/Tab/photobooth.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:video_player/video_player.dart';
 import 'package:slectiv_studio_app/app/modules/login_screen/views/widgets/submit_button.dart';
 import 'package:slectiv_studio_app/utils/constants/colors.dart';
 import 'package:slectiv_studio_app/utils/constants/image_strings.dart';
 import 'package:slectiv_studio_app/utils/constants/text_strings.dart';
-import 'package:url_launcher/url_launcher.dart';
 
-class SlectivPhotobooth extends StatelessWidget {
+class SlectivPhotobooth extends StatefulWidget {
   const SlectivPhotobooth({super.key});
+
+  @override
+  _SlectivPhotoboothState createState() => _SlectivPhotoboothState();
+}
+
+class _SlectivPhotoboothState extends State<SlectivPhotobooth> {
+  late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset(SlectivImages.saveIGVideo);
+    _initializeVideoPlayerFuture = _controller.initialize();
+    _controller.setLooping(true);
+    _controller.play();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,17 +42,56 @@ class SlectivPhotobooth extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(height: 24,),
-              Text(SlectivTexts.photoboothTitle, style: GoogleFonts.spaceGrotesk(textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.w700, color: SlectivColors.blackColor)),),
-              const SizedBox(height: 10,),
-              const Center(child: Image(image: AssetImage(SlectivImages.photoboothImages))),
-              const SizedBox(height: 10,),
-              Text(SlectivTexts.photoboothDescription, style: GoogleFonts.spaceGrotesk(textStyle: const TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: SlectivColors.blackColor)), textAlign: TextAlign.justify,),
-              const SizedBox(height: 20,),
+              const SizedBox(
+                height: 24,
+              ),
+              Text(
+                SlectivTexts.photoboothTitle,
+                style: GoogleFonts.spaceGrotesk(
+                    textStyle: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        color: SlectivColors.blackColor)),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Center(
+                child: FutureBuilder(
+                  future: _initializeVideoPlayerFuture,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      return ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: AspectRatio(
+                          aspectRatio: _controller.value.aspectRatio,
+                          child: VideoPlayer(_controller),
+                        ),
+                      );
+                    } else {
+                      return const CircularProgressIndicator();
+                    }
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                SlectivTexts.photoboothDescription,
+                style: GoogleFonts.spaceGrotesk(
+                    textStyle: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        color: SlectivColors.blackColor)),
+                textAlign: TextAlign.justify,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                
                   // First Feature
                   Container(
                     width: 210,
@@ -42,16 +103,19 @@ class SlectivPhotobooth extends StatelessWidget {
                       ),
                       borderRadius: BorderRadius.circular(5),
                     ),
-                    child: Text(SlectivTexts.photoboothFeature, 
+                    child: Text(
+                      SlectivTexts.photoboothFeature,
                       style: GoogleFonts.spaceGrotesk(
-                        textStyle:const TextStyle(
-                          fontSize: 16, fontWeight: FontWeight.w400, color: SlectivColors.blackColor
-                        ),
+                        textStyle: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                            color: SlectivColors.blackColor),
                       ),
                     ),
                   ),
-                  const SizedBox(height: 5,),
-                
+                  const SizedBox(
+                    height: 5,
+                  ),
                   // Second Feature
                   Container(
                     width: 210,
@@ -63,16 +127,19 @@ class SlectivPhotobooth extends StatelessWidget {
                       ),
                       borderRadius: BorderRadius.circular(5),
                     ),
-                    child: Text(SlectivTexts.widePhotoboxFeature, 
+                    child: Text(
+                      SlectivTexts.widePhotoboxFeature,
                       style: GoogleFonts.spaceGrotesk(
-                        textStyle:const TextStyle(
-                          fontSize: 16, fontWeight: FontWeight.w400, color: SlectivColors.blackColor
-                        ),
+                        textStyle: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                            color: SlectivColors.blackColor),
                       ),
                     ),
                   ),
-                  const SizedBox(height: 5,),
-                
+                  const SizedBox(
+                    height: 5,
+                  ),
                   // Third Feature
                   Container(
                     width: 210,
@@ -80,34 +147,34 @@ class SlectivPhotobooth extends StatelessWidget {
                     alignment: Alignment.centerLeft,
                     decoration: BoxDecoration(
                       border: Border.all(
-                        color: SlectivColors.blackColor.withOpacity(0.1)
-                      ),
+                          color: SlectivColors.blackColor.withOpacity(0.1)),
                       borderRadius: BorderRadius.circular(5),
                     ),
-                    child: Text(SlectivTexts.hightAngleFeature, 
+                    child: Text(
+                      SlectivTexts.hightAngleFeature,
                       style: GoogleFonts.spaceGrotesk(
-                        textStyle:const TextStyle(
-                          fontSize: 16, fontWeight: FontWeight.w400, color: SlectivColors.blackColor
-                        ),
+                        textStyle: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                            color: SlectivColors.blackColor),
                       ),
                     ),
                   ),
-                  const SizedBox(height: 5,),
+                  const SizedBox(
+                    height: 5,
+                  ),
                 ],
               ),
-              const SizedBox(height: 30,),
-              SlectiveWidgetButton(
-                buttonName: SlectivTexts.photoboothButtonName, 
-                onPressed: () async {
-                  if (await canLaunch(SlectivTexts.adminContactUrl)) {
-                    await launch(SlectivTexts.adminContactUrl);
-                  } else {
-                    Get.snackbar("Please Wait..", "Wait for a minute", backgroundColor: SlectivColors.warningColor);
-                  }
-                }, 
-                backgroundColor: SlectivColors.submitButtonColor
+              const SizedBox(
+                height: 30,
               ),
-              const SizedBox(height: 5,),
+              SlectiveWidgetButton(
+                  buttonName: SlectivTexts.photoboothButtonName,
+                  onPressed: () {},
+                  backgroundColor: SlectivColors.submitButtonColor),
+              const SizedBox(
+                height: 40,
+              ),
             ],
           ),
         ),
--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -4,13 +4,18 @@ class SlectivImages {
   // -- App Logos
   static const String applogo = "assets/logos/slectiv_studio_logo.png";
   static const String applogoBlue = "assets/logos/slectiv_studio_logo_blue.png";
-  static const String applogoBlue2 = "assets/logos/slectiv_studio_logo_blue2.png";
+  static const String applogoBlue2 =
+      "assets/logos/slectiv_studio_logo_blue2.png";
 
   // -- Animation
-  static const String onBoardingAnimation1 = "assets/animations/Onboarding1.json";
-  static const String onBoardingAnimation2 = "assets/animations/Onboarding2.json";
-  static const String onBoardingAnimation3 = "assets/animations/Onboarding3.json";
-  static const String waitingToResetPassword = "assets/animations/WaitingToResetPassword.json";
+  static const String onBoardingAnimation1 =
+      "assets/animations/Onboarding1.json";
+  static const String onBoardingAnimation2 =
+      "assets/animations/Onboarding2.json";
+  static const String onBoardingAnimation3 =
+      "assets/animations/Onboarding3.json";
+  static const String waitingToResetPassword =
+      "assets/animations/WaitingToResetPassword.json";
 
   // -- Banners
   static const String newMembershipBanners = "assets/images/hakLoyalty.png";
@@ -23,4 +28,7 @@ class SlectivImages {
 // -- Lines
   static const String line1 = "assets/lines/line1.png";
   static const String line2 = "assets/lines/line2.png";
+// -- video
+  static const String saveIGVideo =
+      "assets/videos/SaveIG.App_3318307591196837453.mp4";
 }
