ca336eefbfbe7632b4d1e0f3ace21058e3216f57
ifan35 <ifaan35@gmail.com>
2024-06-23 22:48:37 +0800

    Pesan commit Anda

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'app/routes/app_pages.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-<<<<<<< HEAD
       options: const FirebaseOptions(
           apiKey: 'AIzaSyAjUU6-7vmZj6Pom-JBxHeQVluJGRnDigA',
           appId: '1:765394348193:android:95cc43c5f923e3d5d2e8e0',
@@ -14,15 +13,6 @@ Future<void> main() async {
           projectId: 'slectiv-studio-project',
           storageBucket: 'slectiv-studio-project.appspot.com'));
 
-=======
-    options: const FirebaseOptions(
-      appId: '1:297855924061:android:4a6b8a3b6b6b9f3f',
-      messagingSenderId: '297855924061',
-      projectId: 'slectiv-studio',
-      apiKey: 'AIzaSyD-4J9Q1Q1J9Q1Q1J9Q1J9Q1J9Q1J9Q1J9',
-    ),
-  );
->>>>>>> 02461b075bde7da4cb2deaa17dd382fb5e24b0fb
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
