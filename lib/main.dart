69c4c71c25644e223c132211ffcb8d390dbf4df5
Rizky Ahsan Syarif <ikkiahsan_@MacBook-Pro.local>
2024-06-23 21:55:56 +0700

    has done

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ Future<void> main() async {
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Slectiv Studio",
+      title: "Slectiv Studio App",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
