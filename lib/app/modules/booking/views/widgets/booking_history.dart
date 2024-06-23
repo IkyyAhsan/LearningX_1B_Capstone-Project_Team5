0801581c4b3bf69bbc224d90d181dd54de7c0099
Rizky Ahsan Syarif <ikkiahsan_@MacBook-Pro.local>
2024-06-23 11:28:14 +0700

    beress wee

--- lib/app/modules/booking/views/widgets/booking_history.dart
+++ lib/app/modules/booking/views/widgets/booking_history.dart
@@ -5,9 +5,10 @@ import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:slectiv_studio_app/app/modules/booking/controllers/booking_controller.dart';
 import 'package:slectiv_studio_app/utils/constants/colors.dart';
 import 'package:slectiv_studio_app/utils/constants/text_strings.dart';
+import 'package:slectiv_studio_app/app/modules/profile/controllers/profile_controller.dart';
 
 class SlectivBookingHistory extends StatefulWidget {
-  const SlectivBookingHistory({Key? key}) : super(key: key);
+  const SlectivBookingHistory({super.key});
 
   @override
   _SlectivBookingHistoryState createState() => _SlectivBookingHistoryState();
@@ -20,6 +21,8 @@ class _SlectivBookingHistoryState extends State<SlectivBookingHistory> {
   @override
   Widget build(BuildContext context) {
     final BookingController bookingController = Get.find();
+    final ProfileController profileController = Get.find();
+    final String userEmail = profileController.email.value;
 
     return Scaffold(
       backgroundColor: SlectivColors.backgroundColor,
@@ -68,13 +71,16 @@ class _SlectivBookingHistoryState extends State<SlectivBookingHistory> {
               List<String> bookings = bookingController.bookings[date] ?? [];
               for (String booking in bookings) {
                 List<String> bookingDetails = booking.split('|');
-                String time = bookingDetails[0];
-                DateTime bookingTime = DateTime(parsedDate.year, parsedDate.month, parsedDate.day, int.parse(time.split(':')[0]), int.parse(time.split(':')[1]));
+                String email = bookingDetails[3];
+                if (email == userEmail) {
+                  String time = bookingDetails[0];
+                  DateTime bookingTime = DateTime(parsedDate.year, parsedDate.month, parsedDate.day, int.parse(time.split(':')[0]), int.parse(time.split(':')[1]));
 
-                if (bookingTime.isAfter(now)) {
-                  upcomingBookings.add({SlectivTexts.bookingDate: date, SlectivTexts.bookingDetails: booking});
-                } else {
-                  completedBookings.add({SlectivTexts.bookingDate: date, SlectivTexts.bookingDetails: booking});
+                  if (bookingTime.isAfter(now)) {
+                    upcomingBookings.add({SlectivTexts.bookingDate: date, SlectivTexts.bookingDetails: booking});
+                  } else {
+                    completedBookings.add({SlectivTexts.bookingDate: date, SlectivTexts.bookingDetails: booking});
+                  }
                 }
               }
             }
