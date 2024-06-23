d1e41a073769696e3651839951fd81da86b0b36a
Rizky Ahsan Syarif <ikkiahsan_@MacBook-Pro.local>
2024-06-23 21:54:17 +0700

    has done

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,11 @@ import 'app/routes/app_pages.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      appId: '1:297855924061:android:4a6b8a3b6b6b9f3f',
-      messagingSenderId: '297855924061',
-      projectId: 'slectiv-studio',
-      apiKey: 'AIzaSyD-4J9Q1Q1J9Q1Q1J9Q1J9Q1J9Q1J9Q1J9',
-    ),
   );
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Slectiv Studio App",
+      title: "Slectiv Studio",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
