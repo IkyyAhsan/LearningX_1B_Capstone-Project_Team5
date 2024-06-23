cf5eb754ee00e258a50c83b8f2d8d5f8fa1b703f
ifan35 <ifaan35@gmail.com>
2024-06-24 01:55:41 +0800

    Pesan commit Anda

--- lib/utils/constants/text_strings.dart
+++ lib/utils/constants/text_strings.dart
@@ -3,7 +3,7 @@ class SlectivTexts {
   static const String mobileApplicationMainTitle = "Slectiv Studio App";
 
   // -- Admin Contact
-  static const String adminContactUrl = "https://wa.me/6281345383641";
+  static const String adminContactUrl = "https://wa.me/+6281345383641";
 
   // -- Splash Text
   static const String splashTextDesc = "Balikpapan 1st Wide Self Photo Studio";
@@ -13,9 +13,12 @@ class SlectivTexts {
   static const String onBoardingTitle2 = "Perfect Photo At all times";
   static const String onBoardingTitle3 = "Capture Your Special Moment";
 
-  static const String onBoardingSubtitle1 = "The place for you to find the best photo studio in the city of Balikpapan";
-  static const String onBoardingSubtitle2 = "High quality photos that will certainly create unforgettable moments";
-  static const String onBoardingSubtitle3 = "Every moment will become a perfect memory at Slectiv Studio";
+  static const String onBoardingSubtitle1 =
+      "The place for you to find the best photo studio in the city of Balikpapan";
+  static const String onBoardingSubtitle2 =
+      "High quality photos that will certainly create unforgettable moments";
+  static const String onBoardingSubtitle3 =
+      "Every moment will become a perfect memory at Slectiv Studio";
 
   // --  Brand Subtitle
   static const String brandSubtitle = "Smile, Click, And Make Everlasting";
@@ -24,9 +27,11 @@ class SlectivTexts {
   static const String loginTitle = "Welcome!";
   static const String loginSubtitle = "Please login to get started";
   static const String registerTitle = "Create Account";
-  static const String registerSubtitle = "Please fill in the following form correctly";
+  static const String registerSubtitle =
+      "Please fill in the following form correctly";
   static const String forgetPasswordTitle = "Forget Password";
-  static const String forgetPasswordSubtitle = "Please fill in the following form correctly";
+  static const String forgetPasswordSubtitle =
+      "Please fill in the following form correctly";
 
   // -- Authentication Form
   static const String email = "Email";
@@ -53,26 +58,42 @@ class SlectivTexts {
 
   // -- Authentication Snackbar
   static const String registerSuccessfullTitle = "Registration Successfull";
-  static const String registerSuccessfullSubtitle = "Congratulations, your account has been successfully created. Please log in to continue.";
-  static const String forgetPasswordWaitingToResetTitle = "Reset Password link has been sent.";
-  static const String forgetPasswordWaitingToResetSubtitle = "Your account security is our priority! We've sent you a password reset link to safely change your password and keep your account protected.";
+  static const String registerSuccessfullSubtitle =
+      "Congratulations, your account has been successfully created. Please log in to continue.";
+  static const String forgetPasswordWaitingToResetTitle =
+      "Reset Password link has been sent.";
+  static const String forgetPasswordWaitingToResetSubtitle =
+      "Your account security is our priority! We've sent you a password reset link to safely change your password and keep your account protected.";
   static const String snackbarThereIsAnErrorTitle = "There is an error";
-  static const String snackbarUnableSendResetLink = "Unable to send password reset link.";
-  static const String snackbarInvalidEmail = "The email you entered is invalid.";
+  static const String snackbarUnableSendResetLink =
+      "Unable to send password reset link.";
+  static const String snackbarInvalidEmail =
+      "The email you entered is invalid.";
   static const String snackbarErrorTitle = "Error";
-  static const String snackbarErrorSubtitle = "Email or Password cannot be empty";
+  static const String snackbarErrorSubtitle =
+      "Email or Password cannot be empty";
   static const String snackbarLoginSuccessfulTitle = "Login Successful";
-  static const String snackbarLoginSuccessfulSubtitle = "Congratulations, You have logged in successfully";
-  static const String snackbarIncorrectEmailSubtitle = "Congratulations, You have logged in successfully";
-  static const String snackbarErrorRegistrationSubtitle = "An error occurred during registration. Please try again.";
-  static const String snackbarErrorUpdateNameSubtitle = "Failed to update name:";
-  static const String snackbarErrorNotCompleteAllColumnSubtitle = "Please complete all columns first.";
-  static const String snackbarErrorNotValidEmailSubtitle = "Please use a valid email format.";
-  static const String snackbarErrorMustConsistsPasswordSubtitle = "The password consists of a minimum of 8 characters.";
-  static const String snackbarSuccessfullyBookingTitle = "Reservation successful";
-  static const String snackbarSuccessfullyBookingSubtitle = "Your booking has been confirmed. Thank you for choosing to use our service.";
+  static const String snackbarLoginSuccessfulSubtitle =
+      "Congratulations, You have logged in successfully";
+  static const String snackbarIncorrectEmailSubtitle =
+      "Congratulations, You have logged in successfully";
+  static const String snackbarErrorRegistrationSubtitle =
+      "An error occurred during registration. Please try again.";
+  static const String snackbarErrorUpdateNameSubtitle =
+      "Failed to update name:";
+  static const String snackbarErrorNotCompleteAllColumnSubtitle =
+      "Please complete all columns first.";
+  static const String snackbarErrorNotValidEmailSubtitle =
+      "Please use a valid email format.";
+  static const String snackbarErrorMustConsistsPasswordSubtitle =
+      "The password consists of a minimum of 8 characters.";
+  static const String snackbarSuccessfullyBookingTitle =
+      "Reservation successful";
+  static const String snackbarSuccessfullyBookingSubtitle =
+      "Your booking has been confirmed. Thank you for choosing to use our service.";
   static const String snackbarBookingConfirmTitle = "Confirm Your Booking";
-  static const String snackbarBookingConfirmSubtitle = "Are you sure you want to proceed with this booking? Please ensure all details are correct.";
+  static const String snackbarBookingConfirmSubtitle =
+      "Are you sure you want to proceed with this booking? Please ensure all details are correct.";
 
   // -- Home Screen
   static const String newAccountType = "Moment Welcomer";
@@ -91,7 +112,8 @@ class SlectivTexts {
   static const String buttonExecutionSubtitle = "Wait for a minute";
 
   // -- Self Photo
-  static const String selfPhotoDescription = "Our Self Photo service aims to showcase your unique personality. Whether you need professional headshots, family portraits, or a personal photo shoot, we create images that reflect the true you. We work with you to showcase your best angles and expressions, ensuring every portrait tells your story.";
+  static const String selfPhotoDescription =
+      "Our Self Photo service aims to showcase your unique personality. Whether you need professional headshots, family portraits, or a personal photo shoot, we create images that reflect the true you. We work with you to showcase your best angles and expressions, ensuring every portrait tells your story.";
   static const String selfPhotoTitle = "Self Photo";
   static const String selfPhotoButtonName = "Booking";
   static const String selfPhotoPrice = "75K";
@@ -105,18 +127,21 @@ class SlectivTexts {
   // -- Wide Photobox
   static const String widePhotoboxTitle = "Wide Photobox";
   static const String widePhotoboxButtonName = "Available Soon";
-  static const String widePhotoboxDescription = "Capture every moment with our Wide Photobox service. Whether it's a family gathering, a corporate event, or a special celebration, our wide-format photography ensures no one is left out of the frame. We bring your events to life with stunning clarity and detail, creating memories that last a lifetime.";
+  static const String widePhotoboxDescription =
+      "Capture every moment with our Wide Photobox service. Whether it's a family gathering, a corporate event, or a special celebration, our wide-format photography ensures no one is left out of the frame. We bring your events to life with stunning clarity and detail, creating memories that last a lifetime.";
 
   // -- Photobooth
   static const String photoboothTitle = "Photo booth";
   static const String photoboothButtonName = "Contact Us";
-  static const String photoboothDescription = "Step into our Photobooth and let the fun begin! Perfect for any event, our Photobooth captures spontaneous moments and candid smiles. With a variety of props and backdrops, you and your guests can create unforgettable memories. Strike a pose and take home a keepsake that will make you smile every time you see it.";
+  static const String photoboothDescription =
+      "Step into our Photobooth and let the fun begin! Perfect for any event, our Photobooth captures spontaneous moments and candid smiles. With a variety of props and backdrops, you and your guests can create unforgettable memories. Strike a pose and take home a keepsake that will make you smile every time you see it.";
   static const String photoboothFeature = "• Photobooth";
 
   // -- Photobooth
   static const String potraitTitle = "Potrait";
   static const String potraitButtonName = "Contact Us";
-  static const String potraitDescription = "Step into our Photobooth and let the fun begin! Perfect for any event, our Photobooth captures spontaneous moments and candid smiles. With a variety of props and backdrops, you and your guests can create unforgettable memories. Strike a pose and take home a keepsake that will make you smile every time you see it.";
+  static const String potraitDescription =
+      "Step into our Photobooth and let the fun begin! Perfect for any event, our Photobooth captures spontaneous moments and candid smiles. With a variety of props and backdrops, you and your guests can create unforgettable memories. Strike a pose and take home a keepsake that will make you smile every time you see it.";
   static const String hours1SessionFeature = "• 1 Hours Session";
   static const String allSoftliteFeature = "• All Softlite";
   static const String printed4RPhotoFeature = "• 4r Printed Photo";
@@ -128,9 +153,12 @@ class SlectivTexts {
   static const String galleryWellcome = "HELLO,";
   static const String galleryWellcome2 = "WELCOME TO";
   static const String galleryHsitory = "HISTORY";
-  static const String galleryHsitoryContex1 = 'Slectiv Studio was founded on February 1 2023 in Balikpapan with a unique concept as "Balikpapan 1st Wide Self Photo Studio". This studio offers a variety of innovative self-photography products, such as Self Photo Photobooth, WidePhotobox, and studio portraits. The main goal of Selective Studio is to provide a unique and enjoyable photography experience for its visitors, with modern and quality facilities.';
-  static const String galleryHistoryContex2 = "With a wide selection of products, Selective Studio provides visitors with the opportunity to create precious moments through photography. Self Photo Photobooth provides visitors with the opportunity to take selfies with various interesting backgrounds, while WidePhotobox makes it possible to take pictures from a wider viewing angle. In addition, visitors can also choose to create professional studio portraits that capture the beauty of important moments in their lives.";
-  static const String galleryHistoryContex3 = "As one of the leading destinations in Balikpapan, Selective Studio has succeeded in attracting the attention of many people with the personal photography experience they offer. With a combination of modern technology and creativity, this studio has become a popular place to capture special moments with a distinctive and unique style.";
+  static const String galleryHsitoryContex1 =
+      'Slectiv Studio was founded on February 1 2023 in Balikpapan with a unique concept as "Balikpapan 1st Wide Self Photo Studio". This studio offers a variety of innovative self-photography products, such as Self Photo Photobooth, WidePhotobox, and studio portraits. The main goal of Selective Studio is to provide a unique and enjoyable photography experience for its visitors, with modern and quality facilities.';
+  static const String galleryHistoryContex2 =
+      "With a wide selection of products, Selective Studio provides visitors with the opportunity to create precious moments through photography. Self Photo Photobooth provides visitors with the opportunity to take selfies with various interesting backgrounds, while WidePhotobox makes it possible to take pictures from a wider viewing angle. In addition, visitors can also choose to create professional studio portraits that capture the beauty of important moments in their lives.";
+  static const String galleryHistoryContex3 =
+      "As one of the leading destinations in Balikpapan, Selective Studio has succeeded in attracting the attention of many people with the personal photography experience they offer. With a combination of modern technology and creativity, this studio has become a popular place to capture special moments with a distinctive and unique style.";
   static const String galleryPhoto = "Photo Gallery";
   static const String gallerysub = "SHARED MEMORY SLECTIV STUDIO";
 
@@ -153,7 +181,8 @@ class SlectivTexts {
   static const String save = "Save";
   static const String exit = "Exit";
   static const String profileSuccessLogoutTitleButton = "Logout Successful";
-  static const String profileSuccessLogoutSubtitleButton = "You have been logged out successfully";
+  static const String profileSuccessLogoutSubtitleButton =
+      "You have been logged out successfully";
   static const String profileId = "id";
   static const String profileName = "name";
   static const String profileImages = "profile_images";
@@ -164,17 +193,22 @@ class SlectivTexts {
   static const String profileNoUserDataFound = "No user data found.";
   static const String profileNoUserLoggedIn = "No user logged in.";
   static const String profileErrorUploadingImage = "Error uploading image:";
-  static const String profileErrorFetchingUserData = "Error fetching user data:";
+  static const String profileErrorFetchingUserData =
+      "Error fetching user data:";
   static const String profileNoImageSelected = "No image selected.";
   static const String profileSuccessFotoChangedTitle = "Photo Changed";
-  static const String profileSuccessFotoChangedSubtitle = "Your profile photo has been successfully changed.";
+  static const String profileSuccessFotoChangedSubtitle =
+      "Your profile photo has been successfully changed.";
   static const String profileSuccessFotoDeletedTitle = "Photo Deleted";
-  static const String profileSuccessFotoDeletedSubtitle = "Your profile photo has been successfully deleted.";
+  static const String profileSuccessFotoDeletedSubtitle =
+      "Your profile photo has been successfully deleted.";
   static const String profileSuccess = "Success";
   static const String profileUpdateNameSubtitle = "Name has been updated";
-  static const String profileUpdatePhoneNumberSubtitle = "Phone Number has been updated";
+  static const String profileUpdatePhoneNumberSubtitle =
+      "Phone Number has been updated";
   static const String profileExitTitle = "Exit";
-  static const String profileConfirmExitMessage = "Are you sure you want to exit?";
+  static const String profileConfirmExitMessage =
+      "Are you sure you want to exit?";
   static const String profileConfirmExit = "Confirm";
   static const String profileConfirmCancel = "Cancel";
 
@@ -187,13 +221,16 @@ class SlectivTexts {
   static const String bookingTitle4 = "Person";
   static const String bookingTitleTime = "Time";
   static const String confirmedBookTitle = "Booking Confirmed!";
-  static const String confirmedBookSubtitle = "Your studio booking has been successfully confirmed. We look forward to seeing you!";
+  static const String confirmedBookSubtitle =
+      "Your studio booking has been successfully confirmed. We look forward to seeing you!";
   static const String selectAnOption = "Select an option...";
   static const String person = "Person";
   static const String color = "Color";
-  static const String chooseDate = "Choose a date above to see available times.";
+  static const String chooseDate =
+      "Choose a date above to see available times.";
   static const String errorBookingValidationTitle = "Error";
-  static const String errorBookingValidationSubtitle = "Please select all options before booking.";
+  static const String errorBookingValidationSubtitle =
+      "Please select all options before booking.";
   static const String bookings = "bookings";
   static const String bookingDate = "date";
   static const String bookingTime = "time";
@@ -201,7 +238,8 @@ class SlectivTexts {
   static const String bookingColor = "color";
   static const String bookingPerson = "person";
   static const String bookingSuccessTitle = "Booking Successful";
-  static const String bookingSuccessDescription = "Your Booking Completed Successfully Placed and We will contact you soon.";
+  static const String bookingSuccessDescription =
+      "Your Booking Completed Successfully Placed and We will contact you soon.";
   static const String bookingSuccessButton = "Back to Home";
   static const String bookingHistoryTitle = "Bookings History";
   static const String bookingNotHistory = "No order history yet";
