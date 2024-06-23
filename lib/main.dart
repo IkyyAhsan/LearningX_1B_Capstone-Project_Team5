02461b075bde7da4cb2deaa17dd382fb5e24b0fb
Rizky Ahsan Syarif <ikkiahsan_@MacBook-Pro.local>
2024-06-23 21:24:14 +0700

    has done

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:intl/date_symbol_data_local.dart';
-import 'package:slectiv_studio_app/utils/constants/text_strings.dart';
 import 'app/routes/app_pages.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initializeDateFormatting(SlectivTexts.profileId, null);
   await Firebase.initializeApp(
-  );       
+    options: const FirebaseOptions(
+      appId: '1:297855924061:android:4a6b8a3b6b6b9f3f',
+      messagingSenderId: '297855924061',
+      projectId: 'slectiv-studio',
+      apiKey: 'AIzaSyD-4J9Q1Q1J9Q1Q1J9Q1J9Q1J9Q1J9Q1J9',
+    ),
+  );
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: SlectivTexts.mobileApplicationMainTitle,
+      title: "Slectiv Studio App",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
